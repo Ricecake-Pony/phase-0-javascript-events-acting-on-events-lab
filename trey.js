aefd2605c27b213c913b87d3644b1cb5582040ac
Trey <trey.delcamp@gmail.com>
2022-09-27 07:53:35 -0500

    commit tree 9ab58647ef61d07515c8e87018f72a9bbbfeb2ac

---
+++ trey.js
@@ -0,0 +1,38 @@
+// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+dodger.style.bottom = "0px";
+dodger.style.left = "100px";
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+  });
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+      moveDodgerRight()
+    }
+  }
+);
+
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumbers, 10);
+  
+    if (right > 0) {
+      dodger.style.left = `${right + 1}px`;
+    }}
